7f7075fdd161108dededadeb07624d2a0ef825bc
Will Iverson <wiverson@gmail.com>
2023-06-16 11:39:59 -0700

    XML doc. Remove redundant auth state notification.

--- Gotrue/PersistenceListener.cs
+++ Gotrue/PersistenceListener.cs
@@ -3,52 +3,62 @@ using Supabase.Gotrue.Interfaces;
 
 namespace Supabase.Gotrue
 {
-    public class PersistenceListener : IGotruePersistenceListener<Session>
-    {
-        public PersistenceListener(IGotrueSessionPersistence<Session> persistence)
-        {
-            Persistence = persistence;
-        }
+	/// <summary>
+	/// Manages the persistence of the Gotrue Session. You'll want to install a persistence listener
+	/// to persist user sessions between app restarts. 
+	/// </summary>
+	public class PersistenceListener : IGotruePersistenceListener<Session>
+	{
+		/// <summary>
+		/// Create a new persistence listener
+		/// </summary>
+		/// <param name="persistence"></param>
+		public PersistenceListener(IGotrueSessionPersistence<Session> persistence)
+		{
+			Persistence = persistence;
+		}
 
-        public IGotrueSessionPersistence<Session> Persistence { get; }
+		public IGotrueSessionPersistence<Session> Persistence { get; }
 
-        public void EventHandler(IGotrueClient<User, Session> sender, Constants.AuthState stateChanged)
-        {
-            switch (stateChanged)
-            {
-                case Constants.AuthState.SignedIn:
-                    if (sender == null)
-                        throw new ArgumentException("Tried to save a null session (1)");
-                    if (sender.CurrentSession == null)
-                        throw new ArgumentException("Tried to save a null session (2)");
+		/// <summary>
+		/// If you install a persistence listener, it will be called when the user signs in and signs out.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="stateChanged"></param>
+		/// <exception cref="ArgumentException"></exception>
+		/// <exception cref="ArgumentOutOfRangeException"></exception>
+		public void EventHandler(IGotrueClient<User, Session> sender, Constants.AuthState stateChanged)
+		{
+			switch (stateChanged)
+			{
+				case Constants.AuthState.SignedIn:
+					if (sender == null)
+						throw new ArgumentException("Tried to save a null session (1)");
+					if (sender.CurrentSession == null)
+						throw new ArgumentException("Tried to save a null session (2)");
 
-                    Persistence.SaveSession(sender.CurrentSession);
-                    break;
-                case Constants.AuthState.SignedOut:
-                    Persistence.DestroySession();
-                    break;
-                case Constants.AuthState.UserUpdated:
-                    if (sender == null)
-                        throw new ArgumentException("Tried to save a null session (1)");
-                    if (sender.CurrentSession == null)
-                        throw new ArgumentException("Tried to save a null session (2)");
+					Persistence.SaveSession(sender.CurrentSession);
+					break;
+				case Constants.AuthState.SignedOut:
+					Persistence.DestroySession();
+					break;
+				case Constants.AuthState.UserUpdated:
+					if (sender == null)
+						throw new ArgumentException("Tried to save a null session (1)");
+					if (sender.CurrentSession == null)
+						throw new ArgumentException("Tried to save a null session (2)");
 
-                    Persistence.SaveSession(sender.CurrentSession);
-                    break;
-                case Constants.AuthState.PasswordRecovery: break;
-                case Constants.AuthState.TokenRefreshed:
-                    if (sender.CurrentSession == null)
-                    {
-                        // If token refresh results in a null session, log out.
-                        EventHandler(sender, Constants.AuthState.SignedOut);
-                    }
-                    else
-                    {
-                        Persistence.SaveSession(sender.CurrentSession);
-                    }
-                    break;
-                default: throw new ArgumentOutOfRangeException(nameof(stateChanged), stateChanged, null);
-            }
-        }
-    }
+					Persistence.SaveSession(sender.CurrentSession);
+					break;
+				case Constants.AuthState.PasswordRecovery: break;
+				case Constants.AuthState.TokenRefreshed:
+					if (sender.CurrentSession != null)
+					{
+						Persistence.SaveSession(sender.CurrentSession);
+					}
+					break;
+				default: throw new ArgumentOutOfRangeException(nameof(stateChanged), stateChanged, null);
+			}
+		}
+	}
 }
